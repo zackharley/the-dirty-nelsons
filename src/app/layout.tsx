8a8d8f4e53b4ded712b08a712c4db0ac3a77d54c
Zack Harley <zackharley@gmail.com>
2025-01-09 17:25:22 -0500

    Add opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from 'next';
 import { Special_Elite } from 'next/font/google';
 import { GoogleAnalytics } from '@next/third-parties/google';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faFacebook,
-  faInstagram,
-  faItunes,
-  faSpotify,
-  faTiktok,
-  faTwitter,
-} from '@fortawesome/free-brands-svg-icons';
 import './globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 config.autoAddCss = false;
 
 const specialElite = Special_Elite({
